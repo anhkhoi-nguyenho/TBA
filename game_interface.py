0ed0acdbcb78a5d4f2d65901a43cf825955416f3
Anh Khoi NGUYEN HO <anhkhoi.nguyenho@edu.esiee.fr>
2025-11-10 16:51:50 +0000

    test n2 du bon fonctionnement du repo

---
+++ game_interface.py
@@ -0,0 +1,8 @@
+# Print text based map, let it be color full ascii or simple text
+# Logic handler :
+# Python checks for terminal color support
+# ASCII encoding with color
+# ASCII encoding without color
+# position indicator -> How to add ?
+
+import game_logic
