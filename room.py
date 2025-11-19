51b548cc65611832bd53358964e6fa1ecf125d29
Anh Khoi NGUYEN HO <anhkhoi.nguyenho@edu.esiee.fr>
2025-11-19 15:05:37 +0000

    Exo - Ajout de docstring pour la classe Room

--- room.py
+++ room.py
@@ -1,6 +1,33 @@
 # Define the Room class.
 
 class Room:
+    """
+    This class is used to define differents places in the game.
+    Each place is identified by its name.
+    A description about the place is also given.
+    This class also defines adjacently connected places.
+
+    Attributes:
+        name (str): The name of the place.
+        description (str): The description about the place.
+        exits (dict) : The dictionnary containing connected places informations.
+
+    Methods:
+        __init__(self, name, description) : The constructor defining attributes.
+        get_exit(self, direction) : Return the exit place corresponding to the given direction
+        get_exit_string(self) : Return a string describing possible exits. In version 1, this string contains possible exit directions.
+        get_long_description(self) : Return a string describing current place including possible exits.
+    
+    Examples:
+
+    >>> room1 = Room("Generic Room 1", "dans Generic room 1 example." )
+    >>> room2 = Room("Generic Room 2", "dans Generic room 2 example." )
+    >>> room1.name
+    'Generic Room 1'
+    >>> room1.description
+    'dans Generic room 1 example.'
+    >>> room1.exits = {"N" : room2, "E" : None, "S" : None, "O" : None}
+    """
 
     # Define the constructor. 
     def __init__(self, name, description):
